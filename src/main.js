baab7395ca4daa15006a5ec7addd96009553d646
flavianohonorato <flaviano.honorato@gmail.com>
2021-04-10 09:10:35 -0300

    add seweetalert pakage

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import router from './router'
 import store from './store'
 import App from './App'
 import VTable from './components/VTable'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 import './utils/axios'
 
 Vue.config.productionTip = false
 Vue.component('v-table', VTable)
+Vue.use(VueSweetalert2)
 
 new Vue({
   router,
